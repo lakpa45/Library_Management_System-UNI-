de02c9ce7e73ac36363226c2653bbb1492b13472
lakpa45 <lakpatshering041@gmail.com>
2026-07-11 12:38:21 +0530

    Fix: Correct filename variable and update route for serving index.html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 
-const __filenam = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('./views/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(PORT, () => {
